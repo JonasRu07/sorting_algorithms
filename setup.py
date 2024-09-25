581e4bf6a6b22421aa61a474d45f2b1ac10dc022
JonasRu07 <rpprtjns@gmail.com>
2024-09-25 18:32:02 +0200

    [ADD] mix function; mixes a given list

--- setup.py
+++ setup.py
@@ -9,3 +9,16 @@ def ord_list(starting_value: int, length: int) -> list:
     for i in range(starting_value, starting_value + length):
         lst.append(i)
     return lst
+
+def mix(lst: list) -> list:
+    """
+    Takes a list and rearranges the elements in a random order
+    USES AND AUXILIARY ARRAY
+    :param lst: any list with any elements, only one Dimension will be shuffled
+    :return: list
+    """
+    ret_lst = []
+    for i in range(0, len(lst)):
+        ret_lst.append(lst.pop(rnd.randint(0, len(lst) - 1)))
+
+    return ret_lst
