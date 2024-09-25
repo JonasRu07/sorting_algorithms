635ea95af13ee78939cd5ed371ce905cc5935c4f
Jonas Ruppert <rpprtjns@gmail.com>
2024-09-25 23:17:24 +0200

    [ADD] main program

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
+import time
 
+import setup
+import sortingAlgorithms
+
+
+print("Setup started")
+main_list = setup.ord_list(1, 1_000)
+main_list = setup.mix(main_list)
+print("Setup complete")
+print("Sorting complete")
+start_time = time.process_time()
+sortingAlgorithms.insertion(main_list)
+print("Sorting complete in: ", time.process_time() - start_time)
+print("Verification started")
+is_sorted = setup.is_truly_sorted(main_list)
+print("List is sorted" if is_sorted else "Sorting failed")
