a8943bf0fdf3b81ba1b243a88c0f0819c27f1ad9
Jonas Ruppert <rpprtjns@gmail.com>
2024-09-26 00:04:19 +0200

    [ADD] selection-sort function;

--- sortingAlgorithms.py
+++ sortingAlgorithms.py
@@ -1,1 +1,18 @@
+def insertion(lst: list) -> list:
+    for i in range(1, len(lst)):
+        element = lst[i]
+        for j in range(0, i-1):
+            if element <= lst[j]:
+                lst = [lst[j]] + lst[:j] +lst[j+1:]
+                break
+    return lst
 
+def selection(lst: list):
+    for i in range(len(lst)):
+        element = lst[i]
+        for j in range(i, len(lst)):
+            if element > lst[j]:
+                element = lst[j]
+        lst.remove(element)
+        lst = lst[:i] + [element] + lst[i:]
+    return lst
