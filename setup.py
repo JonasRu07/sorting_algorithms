a5e13196dd300d770e8e1eb3083c57d52c0f6196
JonasRu07 <rpprtjns@gmail.com>
2024-09-25 18:30:45 +0200

    [ADD] ord_list function; creates an ordered list out of int

--- setup.py
+++ setup.py
@@ -1,1 +1,11 @@
-
+def ord_list(starting_value: int, length: int) -> list:
+    """
+    Creates am ordered list with a given length, beginning at a given value
+    :param starting_value: first (smallest) number in the list
+    :param length: How many elements there are in the list
+    :return list
+    """
+    lst = []
+    for i in range(starting_value, starting_value + length):
+        lst.append(i)
+    return lst
