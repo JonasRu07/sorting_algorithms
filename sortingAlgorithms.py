375c6f5dce3ab8c9286a3e3b713c4aa7df4d74d4
Jonas Ruppert <rpprtjns@gmail.com>
2024-09-26 18:26:59 +0200

    [ADD] quick_rec() function; recursive quick sort function

--- sortingAlgorithms.py
+++ sortingAlgorithms.py
@@ -1,3 +1,6 @@
+import sys
+
+
 def insertion(lst: list) -> list:
     """
     insertion sort algorithm. uses side effects to modify the parameter
@@ -38,3 +41,26 @@ def bubble(lst: list) -> list:
             if lst[j] > lst[j + 1]:
                 lst[j], lst[j+1] = lst[j+1], lst[j]
     return lst
+
+def quick_rec(lst: list) -> list:
+    """
+    recursive quicksort algorithm; Due to recursive limit, breaks at list
+    length of recursive limit from system
+    recursive limit
+    :param lst:
+    :return: sorted_list
+    """
+    if len(lst) > sys.getrecursionlimit():
+        raise "Risk of exceeding the recursive limit"
+    if len(lst) <= 1:
+        return lst
+    pivot = lst[0]
+    smaller_than_pivot = []
+    bigger_than_pivot = []
+    for i in range(1, len(lst)):
+        if lst[i] < pivot:
+            smaller_than_pivot.append(lst[i])
+        else:
+            bigger_than_pivot.append((lst[i]))
+    sorted_list = quick_rec(smaller_than_pivot) + [pivot] + quick_rec(bigger_than_pivot)
+    return sorted_list
